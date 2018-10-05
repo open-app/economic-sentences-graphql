12f923c1324ba2825b57054086cb6c210043303f
luandro <luandro@gmail.com>
2018-10-05 19:26:16 -0300

    Added VF schema

--- schema/typeDefs.js
+++ schema/typeDefs.js
@@ -1,13 +1,68 @@
+const Agent = `
+  type Agent {
+    key: ID
+  }
+`
+
+const Place = `
+  type Place {
+    names: [String]
+    lat: String
+    lng: String
+    note: Note
+  }
+`
+
+const Plan = `
+  type Plan {
+    plannedOn: String
+    due: String
+    note: String
+    name: String
+  }
+`
+
+const ProcessClassification = `
+  type ProcessClassification {
+    note: Note
+    scope: Agent
+    estimatedDuration: String
+  }
+`
+
 
 const Process = `
   type Process {
-
+    scope: Agent
+    plannedStart: String
+    plannedFinish: String
+    isStarted: Boolean
+    isFinished: Boolean
+    processClassifiedAs: ProcessClassification
+    note: Note
   }
 `
 
 const Commitment = `
   type Commitment {
-    
+    action: String
+    process: Process
+    inputOf: Process
+    outputOf: Process
+    provider: Agent
+    receiver: Agent
+    scope: Agent
+    resourceClassifiedAs: ResourceClassification
+    involves: EconomicResource
+    committedQuantity: QuantityValue
+    committedOn: String
+    plannedStart: String
+    due: String
+    isFinished: Boolean
+    plan: Plan
+    isPlanDeliverable: Boolean
+    forPlanDeliverable: Commitment
+    note: Note
   }
 `
 
@@ -35,25 +90,47 @@ const QuantityValue = `
   }
 `
 
-const EconomicEvent = `
-  type EconomicEvent {
-    key: ID!
+const ResourceClassification = `
+  type ResourceClassification {
+    image: String
+    note: Note
+    category: String
+    processCategory: String
   }
 `
 
-// action: String!
-// inputOf: Process
-// outputOf: Process
-// provider: [userId]
-// receiver: [userId]
-// scope: [userId]
-// affects: [EconomicResource]
-// affectedQuantity: QuantityValue
-// start: String
-// url: String
-// requestDistribution: Boolean
-// note: Note
-// fulfills: Fulfillment           
+const EconomicResource = `
+  type EconomicResource {
+    resourceClassifiedAs: ResourceClassification
+    trackingIdentifier: String
+    image: String
+    currentQuantity: QuantityValue
+    note: Note
+    category: String
+    currentLocation: Place
+    createdDate: String
+  }
+`
+
+const EconomicEvent = `
+  type EconomicEvent {
+    key: ID!
+    action: String!
+    inputOf: Process
+    outputOf: Process
+    provider: ID
+    receiver: ID
+    scope: ID
+    affects: [EconomicResource]
+    affectedQuantity: QuantityValue
+    start: String
+    url: String
+    requestDistribution: Boolean
+    note: Note
+    fulfills: Fulfillment
+    
+  }
+`       
 
 const Query = `
   type Query {
@@ -77,4 +154,21 @@ const Schema = () => [`
   }
 `]
 
-module.exports = [Schema, Query, Mutation, EconomicEvent]
+module.exports = [
+  Schema,
+  Query,
+  Mutation,
+  EconomicEvent,
+  Process,
+  ProcessClassification,
+  Agent,
+  Place,
+  Commit,
+  EconomicResource,
+  ResourceClassification,
+  QuantityValue,
+  Unit,
+  Fulfillment,
+  Commitment,
+  Plan,
+]
